56137eef269393fb978e1470730adafbc150a9c7
Stian <69841774+StianPost@users.noreply.github.com>
2021-02-12 11:04:56 +0100

    finished up level 2 and styled it a bit

--- js/question-2.js
+++ js/question-2.js
@@ -2,3 +2,31 @@
 
 const gamesApi =
   'https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating';
+const gamesDiv = document.querySelector('#gamesDiv');
+
+async function getGames(url) {
+  try {
+    const response = await fetch(url);
+    const result = await response.json();
+    const resultArray = result.results;
+    resultArray.forEach((element) => {
+      gamesDiv.innerHTML += `
+        <div class="individualGame">
+        <h2> ${element.name} </h2>
+        <p>Rating: ${element.rating} </p>
+        <p>Tags: ${element.tags.length}</p>
+        `;
+    });
+  } catch (error) {
+    document.querySelector('.alert').innerHTML = showAlertTouser(
+      'Woops, something is wrong, contact the system admin and the monkeys will get to work!',
+      'danger'
+    );
+  } finally {
+    setTimeout(function () {
+      document.querySelector('.alert').innerHTML = '';
+    }, 3000);
+  }
+}
+
+getGames(gamesApi);
