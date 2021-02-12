a623a507c6862866b4cc991ea23ab1ebc35307aa
Stian <69841774+StianPost@users.noreply.github.com>
2021-02-12 17:07:24 +0100

    Added neat stuff

--- js/question-2.js
+++ js/question-2.js
@@ -14,13 +14,52 @@ async function getGames(url) {
       if (i === 8) {
         break;
       }
-      gamesDiv.innerHTML += `
-        <div class="individualGame">
-        <h2> ${resultArray[i].name} </h2>
-        <p><span>Rating:</span> ${resultArray[i].rating} </p>
-        <p><span>Number of Tags:</span> ${resultArray[i].tags.length}</p>
-        <img class="gameImg" src="${resultArray[i].background_image}" alt="Image from the game: ${resultArray[i].name} ">
+
+      let platforms = resultArray[i].platforms;
+      let platformsHTML = '';
+
+      for (let j = 0; j < platforms.length; j++) {
+        platformsHTML += `
+        <p>${j + 1}: ${platforms[j].platform.name}</p>
         `;
+      }
+
+      let ratings = resultArray[i].ratings;
+      let ratingHTML = '';
+      for (let k = 0; k < ratings.length; k++) {
+        ratingHTML += `
+        <div class="ratingsBlock">
+          <p><span>Rating: </span>${ratings[k].title}</p>
+          <p><span>Count: </span>${ratings[k].count}</p>
+          <p><span>Percentage: </span>${ratings[k].percent}%</p>
+        </div>
+        `;
+        if (k === 2) {
+          break;
+        }
+      }
+
+      gamesDiv.innerHTML += `
+        <div class="gamesCard">
+          <div class="gamesCardInfo">
+            <h2> ${resultArray[i].name} </h2>
+            <p><span>Rating:</span> ${resultArray[i].rating} </p>
+            <p><span>Number of Tags:</span> ${resultArray[i].tags.length}</p>
+            <div class="platformDiv"><span>Platforms: </span>${platformsHTML}</div>
+            <img class="gameImg" src="${resultArray[i].background_image}" alt="Image from the game: ${resultArray[i].name} ">
+          </div>
+          <div class="ratings">
+            <div>
+              <h3>Ratings: </h3>
+              
+                ${ratingHTML}
+              
+            </div>
+          </div>
+        </div>
+        `;
+
+      //<p><span>Platform:</span> ${resultArray[i].platforms[j].platform.name}</p>
     }
   } catch (error) {
     document.querySelector('.alert').innerHTML = showAlertTouser(
